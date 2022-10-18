07ba9a921859058f74c3ce182e6d29382ac94b81
brandon-t-harvey <brandontharvey@yahoo.com>
2022-10-18 15:05:46 -0400

    Game history parser completed so far. May need updates for database later. .gitignore file updated

--- audl/audl-containers/game.js
+++ audl/audl-containers/game.js
@@ -2,10 +2,15 @@ class Game{
     gameID;
     awayTeam;
     homeTeam;
+    awayCity;
+    homeCity;
     timestamp;
+    timezone;
     awayScore;
     homeScore;
     averageTemp;
     averageWind;
     averagePrecip;
+    locationName;
+    status;
 }
