14016f90861bd7a6c35c5c1e598a8cc6d77b6975
Philip DiGiorgio <phildigiorgio@gmail.com>
2022-10-25 14:48:41 -0400

    Added URL Parameter Support + Value Retrieval

--- weather/weather-retrieval-script.js
+++ weather/weather-retrieval-script.js
@@ -2,14 +2,30 @@
 
 import fetch from "node-fetch";
 
-const data = getWeatherData();
+gameDataRetrieval();
 
-//Prints the json of a single request from Visual Crossing. Must be automated in the future.
-async function getWeatherData(){
+async function gameDataRetrieval() {
+    //Insert way to retrieve the game data from the database
+
+    //Placeholder values for testing purposes
+    const startTime = "2019-06-13T07:30:00";
+    const endTime = "2019-06-13T09:00:00";
+
+    //location will equal homeTeamCity
+    //Must figure out some way to determine playoff/championship games (Different Locations)
+    const location = "Sterling,VA,US";
+
+    const val = getWeatherData(startTime, endTime, location);
+
+    //Must add way to Insert intervals into database
+
+}
+
+//Retrieves weather data from VisualCrossingAPI depending on start, end, and location parameters
+async function getWeatherData(start, end, loc){
     try {
-        var uri = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=2019-06-13T00:00:00&"
-        + "endDateTime=2019-06-20T00:00:00&unitGroup=uk&contentType=json&dayStartTime=0:0:00&dayEndTime=0:0:00&location=Sterling,VA,US&key=BXJMPGTKDASB9QYG9KQ595BQF";
-
+        var uri = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?combinationMethod=aggregate&aggregateMinutes=15&startDateTime=" + start + "&"
+        + "endDateTime=" + end + "&unitGroup=us&contentType=json&locationMode=single&location=" + loc + "&key=BXJMPGTKDASB9QYG9KQ595BQF";
         const response = await fetch(uri);
 
         if (!response.ok){
@@ -18,13 +34,48 @@ async function getWeatherData(){
 
         const result = await response.json();
         
-        //Printing the response is not ideal at the moment.
-        //Further work will be done in order to print the direct temperature values.
-        console.log(result);
-        return response;
+        const values = processWeatherData(result);
+        return values;
 
     } catch (err) {
         console.log(err);
         console.error(err);
     }
+}
+
+//Processes WeatherData into 15 minute intervals
+//Provides temp, wind speed, precipation, and humidity for each interval
+async function processWeatherData(weatherData){
+    if (!weatherData) {
+        console.log("Empty response");
+        return;
+    }
+    if (weatherData.errorCode>0) {
+        console.log("Error detected. errorCode="+weatherData.errorCode+", message="+weatherData.message);
+        return;
+    }
+
+    var location = weatherData['location'];
+    var values = location['values'];
+    //console.log(values);
+    console.log("Location: "+location.address);
+    let sumTemp = 0;
+    let sumPrecip = 0;
+    let sumWspd = 0;
+    let sumHumidity = 0;
+
+    for (var i=0;i<values.length;i++) {
+        console.log(values[i].datetimeStr+": temp="+values[i].temp+", wspd="+values[i].wspd+", precip="+values[i].precip+ ", humidity="+values[i].humidity);
+        sumTemp += values[i].temp;
+        sumPrecip += values[i].precip;
+        sumWspd += values[i].wspd;
+        sumHumidity += values[i].humidity;
+    }
+    
+    let avgTemp = sumTemp / values.length;
+    let avgPrecip = sumPrecip / values.length;
+    let avgWspd = sumWspd / values.length;
+    let avgHumidity = sumHumidity / values.length;
+    console.log("AvgTemp=" + avgTemp + ", AvgPrecip=" + avgPrecip + ", AvgWspd=" + avgWspd + ", AvgHumidity=" + avgHumidity);
+    return values;
 }
