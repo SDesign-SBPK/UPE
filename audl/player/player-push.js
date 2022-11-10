c80496b664f180ddc8736e4ebe835ae35c883d81
brandon-t-harvey <brandontharvey@yahoo.com>
2022-11-09 19:25:12 -0500

    Pushing JSON files to server completed, still needs to be tested

---
+++ audl/player/player-push.js
@@ -0,0 +1,35 @@
+const controller = require("../../database/controllers/Player.controller")
+const fs = require("fs");
+
+const pushPlayerFiles = function (directory){
+
+    //Callback function for readdir()
+    let pushFile = function (err, files){
+
+        //In case there is an error that pops up
+        if(err){
+            console.log(err)
+        }
+
+        //Take each file from the array and push it to the database
+        files.forEach(file => {
+            //Make the path for each of the files
+            let fullFilePath = directory + '/' + file.name
+
+            //Read the data from the file using the file path into a string format
+            let data = that.fs.readFileSync(fullFilePath).toString()
+
+            //Storing the raw data from the file into a JSON format
+            let json = JSON.parse(data)
+
+            //Use the database controller to push the JSON file
+            controller.createPlayer(json)
+        })
+    }
+
+    fs.readdir(directory,{withFileTypes: true}, pushFile)
+}
+
+
+
+module.exports ={pushPlayerFiles}
