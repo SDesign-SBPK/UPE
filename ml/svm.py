21d5e4fa176dd7a056f91ecb0edce6cda0ff2cb1
brandon-t-harvey <brandontharvey@yahoo.com>
2022-11-28 15:29:46 -0500

    Example working ML prediction based on season wins

--- ml/svm.py
+++ ml/svm.py
@@ -1,4 +1,24 @@
-from sklearn import svm
+from array import array
 
-def predict(teamOne, teamTwo):
-    print("Team One: " + teamOne + "Team Two: " + teamTwo)
+from sklearn import svm, datasets
+
+def predict():
+    #Teams Union and Flyers stats. This will eventually change to take team parameters and use those to pull data from the server.
+    teamStats = [[0.389], [0.576], [0.568], [0.647], [0.583], [0.595], [0.519], [0.605], [0.468], [0.511], [0.533], [0.512], [0.583], [0.533], [0.703],
+                 [0.421], [0.524], [0.551], [0.645], [0.529], [0.605], [0.550], [0.605], [0.488], [0.591], [0.529], [0.519], [0.571], [0.594], [0.533]]
+    targets = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+               3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+    machine = svm.SVC(kernel="linear", C=1)
+    machine.fit(teamStats, targets)
+    result = machine.predict([[0.6]])
+    print(result)
+
+
+
+def test():
+    iris = datasets.load_iris()
+    print(iris.data[:])
+    print(iris.target)
+
+
+predict()
