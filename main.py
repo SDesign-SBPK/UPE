e777c9377138c8d1638d1e119dd9774467a459ae
KarimAlami01 <karimalami@gwu.edu>
2022-12-05 15:51:09 -0500

    connection to input page

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ app = Flask('app')
 def index():
   return render_template('index.html') 
 
+@app.route('/input', methods=['GET', 'POST'])
+def input():
+  return render_template('input.html')
+
 app.run(host='0.0.0.0', port=8080)
