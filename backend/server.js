bd724c9cb766862079b22f2078bfa951efeee092
Philip DiGiorgio <phildigiorgio@gmail.com>
2022-12-12 18:22:15 -0500

    Static Files + Mysql Backend support

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const connection = require("../database/connection.json");
+
+const con = mysql.createConnection({
+	host: connection.host,
+	user: connection.user,
+	password: connection.pass,
+	database: connection.database
+  });
 
 const app = express();
 
+//Port 8080
+const PORT = 8080;
+/*process.env.PORT*/ 
+
 //Build in middleware to handle urlencoded data (form data)
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Built in middleware for json
 app.use(express.json());
 
 //Used to serve static files
-//app.use(express.static(path.join(__dirname, 'someFolder')));
+app.use(express.static(path.join(__dirname, '/../static')));
 
 //Simple route for home page - file path will be changed
 app.get("/", (req, res) => {
@@ -24,15 +38,18 @@ app.get("/Upcoming-Games", (req, res) => {
 
 //Form Pages - need to be added
 app.get("/Prediction-Form", (req, res) => {
-	res.send("test");
+	res.sendFile("./input.html", { root: __dirname });
 });
 
 app.post("/Prediction-Form", (req, res) => {
-	res.send("test");
+	console.log(req.body);
+	let homeTeam = req.body.dropdown1;
+	let awayTeam = req.body.dropdown2;
+	let windspeed = req.body.wind;
+
+	//Send homeTeam and awayTeam to ML
+
+	res.sendFile("./output.html", { root: __dirname });
 });
 
-//Port 8080
-const port = 8080;
-/*process.env.PORT*/ 
-
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
