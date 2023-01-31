feb23a1c1d53143e20bc485f016ed0c8149842e9
Philip DiGiorgio <phildigiorgio@gmail.com>
2023-01-31 11:51:41 -0500

    Upcoming Games Display + Upc. Games Scraper Structure

--- web_app/backend/server.js
+++ web_app/backend/server.js
@@ -35,80 +35,43 @@ app.get("/", (req, res) => {
 });
 
 app.get("/Upcoming-Games", (req, res) => {
-	let mock_data = [
-		{
-			"homeTeam": "empire",
-			"awayTeam": "breeze",
-			"startTime": "December 16, 2022 6:00 PM"
-		},
-		{
-			"homeTeam": "flyers",
-			"awayTeam": "legion",
-			"startTime": "December 16, 2022, 7:00 PM"
-		},
-		{
-			"homeTeam": "hustle",
-			"awayTeam": "cannons",
-			"startTime": "December 17, 2022, 3:00 PM"
-		},
-		{
-			"homeTeam": "royal",
-			"awayTeam": "rush",
-			"startTime": "December 17, 2022 6:30 PM"
-		},
-		{
-			"homeTeam": "union",
-			"awayTeam": "radicals",
-			"startTime": "December 17, 2022 6:30 PM"
+	
+	con.query('SELECT gameID, , homeTeam, startTime, winner FROM predictedgames', (err, rows, fields) => {
+		if (err) throw err;
+		
+		games = rows;
+
+		let html_string = `<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="utf-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1">
+						<title>Upcoming Games</title>
+						<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+			</head>
+			<body>
+			<input type = "button" onclick="document.location='/'" value = "Home" class = "btn btn-link">
+			<div class = "container">
+			<h1>Upcoming Games</h1>
+			<div class = "row border-bottom">
+				<div class = "col-sm-3">Away</div>
+				<div class = "col-sm-3">Home</div> 
+				<div class = "col-sm-3">Time</div>
+				<div class = "col-sm-3">Predicted Winner</div>
+			</div>`;
+
+			// Build table entries for each game
+		for (let i = 0; i< games.length; i++){
+			html_string += `<div class = "row">
+				<div class = "col-sm-2">${games[i].awayTeam}</div>
+				<div class = "col-sm-2">${games[i].homeTeam}</div>
+				<div class = "col-sm-2">${games[i].startTime}</div>
+				<div class = "col-sm-3">${games[i].winner}</div>
+			</div>`;
 		}
-	];
-
-	let html_string = `<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1">
-					<title>Upcoming Games</title>
-					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-		</head>
-		<body>
-		<input type = "button" onclick="document.location='/'" value = "Home" class = "btn btn-link">
-		<div class = "container">
-		<h1>Upcoming Games</h1>
-		<div class = "row border-bottom">
-			<div class = "col-sm-3">Away</div>
-			<div class = "col-sm-3">Home</div> 
-			<div class = "col-sm-3">Time</div>
-			<div class = "col-sm-3">Predicted Winner</div>
-		</div>`;
-
-	// Call for a prediction of each game
-	mock_data.forEach((game) => {
-		const url_object = {
-			team1: game.homeTeam,
-			team2: game.awayTeam,
-			wind_speed: 3
-		};
-		const url_args = querystring.stringify(url_object);
-		let prediction = http.get("http://localhost:50300/api/v1/predict/teams/?" + url_args, response => {
-			let data = "";
-			response.on("data", chunk => {data += chunk});
-			response.on("end", () => {
-				console.log(data);
-				game.winner = JSON.parse(data).winner;
 				
-				html_string += `<div class = "row">
-					<div class = "col-sm-3">${game.awayTeam}</div>
-					<div class = "col-sm-3">${game.homeTeam}</div> 
-					<div class = "col-sm-3">${game.startTime}</div>
-					<div class = "col-sm-3">${game.winner}</div>
-					</div>`;
-				html_string += `</div></body></html>`;
-			});
-		});
+		res.send(html_string);
 	});
-
-	res.send(html_string);
 });
 
 //This route showcases the ten games with the highest average wind speed
