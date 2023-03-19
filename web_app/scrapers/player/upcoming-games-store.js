318254011b3b1f5fe42b3bf1e73ff5a81b1bc9a2
Philip DiGiorgio <phildigiorgio@gmail.com>
2023-03-19 10:26:20 -0400

    Added query to automatically remove old weather intervals

--- web_app/scrapers/player/upcoming-games-store.js
+++ web_app/scrapers/player/upcoming-games-store.js
@@ -32,6 +32,11 @@ const savePageJson = async function (res) {
         if (err) throw err;
     });
 
+    //Deletes all previously calculated weather intervals from DB
+    await con.query('DELETE weatherintervals FROM weatherintervals INNER JOIN games ON weatherintervals.gameID = games.gameID WHERE games.status = "Upcoming"', (err, rows, fields) => {
+        if (err) throw err;
+    });
+
     //Deletes Any leftover upcoming games from DB
     await con.query('DELETE FROM games WHERE status = "Upcoming"', (err, rows, fields) => {
         if (err) throw err;
